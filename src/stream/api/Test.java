2fdb9e33ee16d5589d402136e3da0bcc9d4726e0
Chahid Adnane <adnanechahid@gmail.com>
2022-06-24 22:13:17 +0100

    feat(): display the first three even numbers squared

--- src/stream/api/Test.java
+++ src/stream/api/Test.java
@@ -27,5 +27,22 @@ public class Test {
                 .mapToInt(i -> i)
                 .sum();
         System.out.println(sommeM3);
+
+
+        List<String> caracteresList = Arrays.asList("a1", "a2", "b2", "b1", "c1");
+        caracteresList.stream()
+                .filter(c -> c.startsWith("b"))
+                .sorted()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+
+
+        // trois premier nombres pairs au carre
+        List<Integer> nombres = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        nombres.stream()
+                .filter(nombre -> nombre % 2 == 0)
+                .map(nombre -> (int) Math.pow(nombre, 2))
+                .limit(3)
+                .forEach(System.out::println);
     }
 }
