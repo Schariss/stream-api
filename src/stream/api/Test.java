253b7fc86c52856477bd5e8b0b5c313bfef87577
Chahid Adnane <adnanechahid@gmail.com>
2022-06-24 19:29:18 +0100

    feat(): sum of numbers less than 10

---
+++ src/stream/api/Test.java
@@ -0,0 +1,24 @@
+package stream.api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> entiers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+        // Sum of numbers less than 10
+        // First Method
+        long sommeM1 = entiers.stream()
+                .filter(v -> v < 10)
+                        .reduce((fstnb, secnb) -> fstnb + secnb).get();
+        System.out.println(sommeM1);
+
+        // Second Method
+        long sommeM2 = entiers.stream()
+                .filter(v -> v < 10)
+                .mapToInt(i -> i)
+                .sum();
+        System.out.println(sommeM2);
+    }
+}
